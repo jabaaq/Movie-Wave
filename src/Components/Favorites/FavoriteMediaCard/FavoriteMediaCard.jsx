5cb58078fbc783bd2244ff47dc35fc0a31a28e75
Jaba <jabaqadagishvili4@gmail.com>
2024-02-18 17:04:53 +0400

    added an edit mode to the Favorites page, which will allow to remove media from the favorites list

--- src/Components/Favorites/FavoriteMediaCard/FavoriteMediaCard.jsx
+++ src/Components/Favorites/FavoriteMediaCard/FavoriteMediaCard.jsx
@@ -1,13 +1,34 @@
 import "./FavoriteMediaCard.scss";
 import Tilt from "react-parallax-tilt";
-import { BsDot } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { BsDot } from "react-icons/bs";
+import { IoCloseOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
 
-const FavoriteMediaCard = ({ media }) => {
+const FavoriteMediaCard = ({
+  media,
+  editMode,
+  currItem,
+  storagedFavoriteList,
+}) => {
   const { id, title, poster, release_date, mediaType } = media;
+
+  localStorage.getItem("favoriteMedia");
+  const handleMediaRemove = () => {};
+
   return (
     <div className="favorite_media_card_container">
-      <Link to={`/${mediaType.toLowerCase()}/${id}`}>
+      <button
+        className={`remove_from_favorites_btn ${editMode ? "" : "none"}`}
+        onClick={handleMediaRemove}
+      >
+        <IoCloseOutline size={30} />
+      </button>
+      <Link
+        to={`/${mediaType.toLowerCase()}/${id}`}
+        className={`${editMode ? "disabled_link" : ""}`}
+      >
         <Tilt
           tiltEnable={false}
           glareEnable={true}
@@ -18,6 +39,9 @@ const FavoriteMediaCard = ({ media }) => {
         >
           <div className="favorite_media_card">
             <div className="favorite_media_card_content">
+              <div
+                className={`card_edit_mode ${editMode ? "block" : ""}`}
+              ></div>
               <img src={poster} alt={`${title}'s Poster`} />
               <h4 className="favorite_media_name">
                 {title.length > 15 ? title.slice(0, 15) + "..." : title}
--- src/Components/Favorites/Favorites.jsx
+++ src/Components/Favorites/Favorites.jsx
@@ -1,28 +1,43 @@
 import "./Favorites.scss";
-import Footer from "../Footer/Footer";
-import { FaHeart } from "react-icons/fa";
 import FavoriteMediaCard from "./FavoriteMediaCard/FavoriteMediaCard";
-import { useEffect } from "react";
+import Footer from "../Footer/Footer";
+import { useEffect, useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
 
 const Favorites = () => {
+  const [editMode, setEditMode] = useState(false);
+
   const storagedFavoriteList = JSON.parse(
     localStorage.getItem("favoriteMedia")
   );
 
-  useEffect(() => {
-    console.log(storagedFavoriteList);
-  }, []);
-
   return (
     <>
       <div className="favorites_container">
-        <h2>
-          <FaHeart /> FAVORITES
-        </h2>
+        <div className="favorites_container_header">
+          <h2>
+            <FaHeart /> FAVORITES
+          </h2>
+          <button
+            className={`edit_fav_list_btn ${editMode ? "clicked" : ""}`}
+            onClick={() => setEditMode(!editMode)}
+          >
+            <div viewBox="0 0 448 512" className="edit_icon">
+              <MdEdit size={20} />
+            </div>
+          </button>
+        </div>
         <div className="favorite_media_cards_container">
           {storagedFavoriteList &&
             storagedFavoriteList.map((media, i) => (
-              <FavoriteMediaCard key={i} media={media} />
+              <FavoriteMediaCard
+                key={i}
+                currItem={i}
+                media={media}
+                editMode={editMode}
+                storagedFavoriteList={storagedFavoriteList}
+              />
             ))}
         </div>
       </div>
